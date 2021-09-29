c0764c3404cec8644bc4a3ff55666cc16643a343
Ananya <avagadi15@gmail.com>
2021-09-29 22:53:21 +0530

    improvising, removed bs accordion, cstm css linkd

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,15 @@
+$( document ).on('turbolinks:load', function() {
+ function toggleIcon(e) {
+        $(e.target)
+            .prev('.panel-heading')
+            .find(".more-less")
+            .toggleClass('glyphicon-plus glyphicon-minus');
+    }
+    $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+    $('.panel-group').on('shown.bs.collapse', toggleIcon);
+});
+
+
+
+
+
